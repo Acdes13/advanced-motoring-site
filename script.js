6d5c341be7dce471e282c03fad0e4b764e018ed7
Abbie <abbie@example.com>
2025-06-04 22:40:40 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thanks for contacting Advanced Motoring School! We'll get back to you soon.");
+  });
+  
